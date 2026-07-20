3964b928270cb021e4a6fab9720c9847646144c8
Winver10 <cty1072011@outlook.com>
2026-07-20 14:35:39 +0800

    Modified creat-cmake.sh Support C language CMake Project Generating

--- creat-cmake.sh
+++ creat-cmake.sh
@@ -11,7 +11,20 @@ while true; do
     fi
 done
 
-echo "This porject will use cxx 17"
+PS3="Choose your language: "
+
+select lang in C CXX; do
+    echo "The language is $lang"
+    break
+done
+
+if [[ "$lang" == "C" ]]; then
+    filename="main.c"
+elif [[ "$lang" == "CXX" ]]; then
+    filename="main.cpp"
+fi
+
+echo "This porject will use $lang 17"
 
 mkdir "$projectname"
 echo "Created the projcet directry"
@@ -24,9 +37,9 @@ echo "Created the CMakeLists.txt"
 mkdir src
 mkdir include
 
-touch src/main.cpp
-echo "Created the main.cpp"
-printf "cmake_minimum_required(VERSION 3.10)\nproject($projectname)\nset(CMAKE_EXPORT_COMPILE_COMMANDS ON)\nset(CMAKE_CXX_STANDARD 17)\nset(CMAKE_CXX_STANDARD_REQUIRED ON)\nadd_executable($projectname src/main.cpp)\ntarget_include_directories($projectname PRIVATE include)"  > CMakeLists.txt
+touch src/$filename
+echo "Created the src/$filename"
+printf "cmake_minimum_required(VERSION 3.10)\nproject($projectname)\nset(CMAKE_EXPORT_COMPILE_COMMANDS ON)\nset(CMAKE_$lang_STANDARD 17)\nset(CMAKE_$lang_STANDARD_REQUIRED ON)\nadd_executable($projectname src/$filename)\ntarget_include_directories($projectname PRIVATE include)"  > CMakeLists.txt
 
 mkdir build
 cd build
